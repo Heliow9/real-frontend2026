131323b1142ad7fbdd903fb6fc91d1351026b95b
Helio Livramento <helio.paraiso@gmail.com>
2026-04-24 00:01:53 -0300

    corrigindo route api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://real-backend-2026.onrender.com',
+  baseURL: process.env.REACT_APP_API_URL || 'https://real-backend-2026-1.onrender.com',
   timeout: 45000,
 });
 
